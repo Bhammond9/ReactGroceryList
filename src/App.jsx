13049fabe8f740b1a3ce1eff5a21cf869297b93d
Bhammond9 <bhammond901@yahoo.com>
2025-02-10 19:22:56 -0700

    built jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import groceryCartImg from "./assets/grocery-cart.png"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <main className="App">
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h4 className="success">You're Done</h4>
+        <div className="header">
+          <h1>Shopping List</h1>
+          <img src={groceryCartImg} alt="" />
+          <input
+            type="text"
+            placeholder="Add an Item"
+            className="item-input"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <ul>
+        <li>
+          <div className="container">
+            <input type="checkbox" />
+            <p>Carrots</p>
+          </div>
+          <div>
+            <button className="remove-button">X</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </main>
 
-export default App
+  );
+}
+export default App;
